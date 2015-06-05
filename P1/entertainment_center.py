91573c0f01a1ae51bf850edf0d080278c571ca99
Shien Jin Ong <shienjin@gmail.com>
2015-06-05 17:17:28 +0800

    add comments & lint-pep8

--- P1/entertainment_center.py
+++ P1/entertainment_center.py
@@ -1,12 +1,15 @@
 import media
 import fresh_tomatoes
 
-gramm = media.Movie(title="1001 Gramm", 
-	trailer_youtube_url="https://www.youtube.com/watch?v=FVIAtIHcehM",
-	poster_image_url="http://t2.gstatic.com/images?q=tbn:ANd9GcQuEzifstiKe2hcfhlG9XrOY8gwgYLi0bydQyfifNT4btRqoAj4")
+gramm = media.Movie(
+            title="1001 Gramm",
+            trailer_youtube_url="https://www.youtube.com/watch?v=FVIAtIHcehM",
+            poster_image_url="http://goo.gl/q6y0Q3")
 
-chocolat = media.Movie(title="Chocolat", 
-	trailer_youtube_url="https://www.youtube.com/watch?v=zH3dNtJGcn8",
-	poster_image_url="http://t2.gstatic.com/images?q=tbn:ANd9GcSAA-9RVzQYsZFzKRsGrOnQnrK1OuNf3GFPstf_JpIvvn2Zrtks")
+chocolat = media.Movie(
+            title="Chocolat",
+            trailer_youtube_url="https://www.youtube.com/watch?v=zH3dNtJGcn8",
+            poster_image_url="http://goo.gl/59Xcv8")
 
+# creates HTML of movies page and opens it in a browser
 fresh_tomatoes.open_movies_page([gramm, chocolat])
--- P1/media.py
+++ P1/media.py
@@ -1,5 +1,11 @@
 class Movie:
-	def __init__(self, trailer_youtube_url, title, poster_image_url):
-		self.title = title
-		self.trailer_youtube_url = trailer_youtube_url
-		self.poster_image_url = poster_image_url
+    """
+    Attributes:
+        title (str): movie title
+        trailer_youtube_url (str): youtube url of movie trailer
+        poster_image_url (str): image url of movie poster
+    """
+    def __init__(self, trailer_youtube_url, title, poster_image_url):
+        self.title = title
+        self.trailer_youtube_url = trailer_youtube_url
+        self.poster_image_url = poster_image_url
